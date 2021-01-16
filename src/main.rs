3b2a2ee83527be66a8dc2ae92a7baf17f839f00c
srirajshukla <srirajshukla@gmail.com>
2021-01-16 13:37:53 +0530

    added index page and rocket calling function.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
 #![feature(proc_macro_hygiene, decl_macro)] // for using the nightly version
-
 #[macro_use] extern crate rocket;
 
+use rocket::Rocket;
+
+#[get("/")]
+fn index() -> &'static str{
+    "
+    USAGE
+
+        POST /
+
+            accepts raw data in the body of the requests and responds
+            with a URL of a page containing the body's content.
+
+        GET /<id>
+
+            retrieves the content for the paste with id `<id>`
+    "
+}
+
+fn rocket() -> Rocket{
+    rocket::ignite().mount("/", routes![index])
+}
+
 fn main() {
-    rocket::ignite().launch();
+    rocket().launch();
 }
